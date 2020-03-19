2bd00e6c049938250277a3575d0f78e31f188dc3
Vitaliy Podrez <tamtamlg@gmail.com>
2020-03-19 13:38:44 +0200

    remove js and img from block.js

--- block.js
+++ block.js
@@ -2,19 +2,18 @@
 
 // Требует node.js и пакета mkdirp
 
-const fs = require('fs');                                                       // будем работать с файловой системой
-const mkdirp = require('mkdirp');                                               // зависимость, должна быть установлена (см. описание выше)
+const fs = require('fs');                          // будем работать с файловой системой
+const mkdirp = require('mkdirp');                  // зависимость, должна быть установлена (см. описание выше)
 
-let blockName = process.argv[2];                                                // получим имя блока
-let defaultExtensions = ['scss', 'html', 'img'];                                // расширения по умолчанию
-let extensions = uniqueArray(defaultExtensions.concat(process.argv.slice(3)));  // добавим введенные при вызове расширения (если есть)
+let blockName = process.argv[2];                   // получим имя блока
+let extensions = ['scss', 'html'];                 // расширения создаваемых файлов
 let styleManagerPath = './src/scss/style.scss';
 
 // Если есть имя блока
 if(blockName) {
 
-  let dirPath = `./src/blocks/${blockName}/`; // полный путь к создаваемой папке блока
-  mkdirp(dirPath, function(err){                   // создаем
+  let dirPath = `./src/blocks/${blockName}/`;      // полный путь к создаваемой папке блока
+  mkdirp(dirPath, (err) => {                       // создаем
 
     // Если какая-то ошибка — покажем
     if(err) {
@@ -26,9 +25,9 @@ if(blockName) {
       console.log(`Создание папки ${dirPath} (создана, если ещё не существует)`);
 
       // Обходим массив расширений и создаем файлы, если они еще не созданы
-      extensions.forEach(function(extention){
+      extensions.forEach((extention) => {
 
-        let filePath;                                         // полный путь к создаваемому файлу
+        let filePath = '';                                    // полный путь к создаваемому файлу
         if (extention === 'scss') {
           filePath = `${dirPath}_${blockName}.${extention}`;
         } else {
@@ -46,7 +45,7 @@ if(blockName) {
           let connectManager = fs.readFileSync(styleManagerPath, 'utf8');
 
           // Делаем из строк массив, фильтруем массив, оставляя только строки с незакомментированными импортами
-          let fileSystem = connectManager.split('\n').filter(function(item) {
+          let fileSystem = connectManager.split('\n').filter((item) => {
             if(/^(\s*)@import/.test(item)) return true;
             else return false;
           });
@@ -58,7 +57,7 @@ if(blockName) {
           let impotrtExist = false;
 
           // Обойдём массив и проверим наличие импорта
-          for (var i = 0, j=fileSystem.length; i < j; i++) {
+          for (let i = 0, j=fileSystem.length; i < j; i++) {
             if(reg.test(fileSystem[i])) {
               impotrtExist = true;
               break;
@@ -68,11 +67,11 @@ if(blockName) {
           // Если флаг наличия импорта по-прежнему опущен, допишем импорт
           if(!impotrtExist) {
             // Открываем файл
-            fs.open(styleManagerPath, 'a', function(err, fileHandle) {
+            fs.open(styleManagerPath, 'a', (err, fileHandle) => {
               // Если ошибок открытия нет...
               if (!err) {
                 // Запишем в конец файла
-                fs.write(fileHandle, styleFileImport + '\n', null, 'utf8', function(err, written) {
+                fs.write(fileHandle, styleFileImport + '\n', null, 'utf8', (err) => {
                   if (!err) {
                     console.log(`В ${styleManagerPath} записано: ${styleFileImport}`);
                   } else {
@@ -89,32 +88,14 @@ if(blockName) {
           }
         }
 
-        // Если это JS
-        else if(extention == 'js') {
-          fileContent = `// (function(){\n// код\n// }());\n`;
-        }
-
         // Если это html
         else if(extention == 'html') {
           fileContent = `<!-- ${blockName} -->\n<div class="${blockName}">\n\n</div>\n<!-- /${blockName} -->`;
         }
 
-        // Если нужна подпапка для картинок
-        else if (extention === 'img') {
-          const imgFolder = `${dirPath}img/`;
-          if (fileExist(imgFolder) === false) {
-            mkdirp(imgFolder, (err) => {
-              if (err) console.error(err);
-              else console.log(`Создание папки: ${imgFolder} (если отсутствует)`);
-            });
-          } else {
-            console.log(`Папка ${imgFolder} НЕ создана (уже существует) `);
-          }
-        }
-
         // Создаем файл, если он еще не существует
-        if(fileExist(filePath) === false && extention !== 'img') {
-          fs.writeFile(filePath, fileContent, function(err) {
+        if(fileExist(filePath) === false) {
+          fs.writeFile(filePath, fileContent, (err) => {
             if(err) {
               return console.log(`Файл НЕ создан: ${err}`);
             }
@@ -132,15 +113,6 @@ else {
   console.log('Отмена операции: не указан блок');
 }
 
-// Оставить в массиве только уникальные значения (убрать повторы)
-function uniqueArray(arr) {
-  var objectTemp = {};
-  for (var i = 0; i < arr.length; i++) {
-    var str = arr[i];
-    objectTemp[str] = true; // запомнить строку в виде свойства объекта
-  }
-  return Object.keys(objectTemp);
-}
 
 // Проверка существования файла
 function fileExist(path) {
